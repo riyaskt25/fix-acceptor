0f701dbb230890c81d6e0ef695c796dd9f1c8331
riyaskt25 <riyaskt25@gmail.com>
2026-07-08 17:35:14 +0530

    changes

--- src/test/java/com/demo/fix/acceptor/FixAcceptorApplicationTests.java
+++ src/test/java/com/demo/fix/acceptor/FixAcceptorApplicationTests.java
@@ -3,7 +3,7 @@ package com.demo.fix.acceptor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = "fix.acceptor.enabled=false")
+@SpringBootTest(properties = "fix.enabled=false")
 class FixAcceptorApplicationTests {
 
 	@Test
